ff3133519ce71a6c92c26676bd1be8bb53ce79f9
TaylorChris2 <chris.taylor2480@gmail.com>
2024-10-27 13:29:24 -0500

    for sent

--- sentiment.py
+++ sentiment.py
@@ -6,6 +6,10 @@ from dotenv import load_dotenv
 from urllib.request import urlopen
 from urllib.request import Request
 from datetime import datetime, timedelta
+from nltk.sentiment import SentimentAnalyzer
+from nltk.sentiment.vader import SentimentIntensityAnalyzer
+import nltk
+nltk.download('vader_lexicon')
 
 
 stock = 'AAPL'
@@ -15,3 +19,20 @@ file_path = os.path.join(directory_name, filename)
 df = pd.read_csv(file_path)
 print(df.head())
 print("Test")
+
+## iterate through the dataset, headline collum, get each headline that will be he sentance
+#iterate through data set
+#ss is array of score.
+scores = []
+for sentence in df["headline"]:
+    sid = SentimentIntensityAnalyzer()
+    ss = sid.polarity_scores(sentence)
+    sorted_ss = sorted(ss)
+     #add a collum
+    for k in sorted(ss):
+        scores.append(ss[k])
+        break
+    
+df["sentiment_score"] = scores
+print(df.head())
+print(df["sentiment_score"])
