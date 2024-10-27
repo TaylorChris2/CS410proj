9f8632e4af145ae2e62d233326805dc1f895d1ab
ellisirwin <irwin.ellis@gmail.com>
2024-10-27 13:47:30 -0500

    merged news and price data

--- prices.py
+++ prices.py
@@ -12,5 +12,22 @@ import matplotlib.pyplot as plt
 aapl = yf.Ticker("AAPL")
 hist = aapl.history("1y")
 
-plt.plot(hist["Open"])
+hist["timestamp"] = hist.index
+hist['timestamp'] = hist['timestamp'].apply(lambda x: x.strftime('%Y-%m-%d'))
+print(hist.head())
+
+news_data = pd.read_csv("data/AAPL.csv")
+news_data['timestamp'] = news_data['timestamp'].apply(lambda x: x[:10])
+print(news_data.head())
+
+news_and_prices = pd.merge(hist, news_data, on='timestamp', how='inner')
+print(news_and_prices.head())
+print(news_and_prices.shape)
+
+agg_prices = news_and_prices.groupby("timestamp").agg({"Open" : "max"})
+print(agg_prices.head())
+print(agg_prices.shape)
+
+# plt.plot(hist["Open"])
+plt.plot(agg_prices["Open"])
 plt.show()
