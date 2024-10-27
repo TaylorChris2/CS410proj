7fb932aa0f4f6354d68548abe182448441d29123
khushal <gentela2@illinois.edu>
2024-10-27 12:55:43 -0500

    added sentiment.py

---
+++ sentiment.py
@@ -0,0 +1,16 @@
+import pandas as pd
+import numpy as np
+import finnhub
+import os
+from dotenv import load_dotenv
+from urllib.request import urlopen
+from urllib.request import Request
+from datetime import datetime, timedelta
+
+
+stock = 'AAPL'
+directory_name = 'data'
+filename = f"{stock}.csv"
+file_path = os.path.join(directory_name, filename)
+df = pd.read_csv(file_path)
+print(df.head())
